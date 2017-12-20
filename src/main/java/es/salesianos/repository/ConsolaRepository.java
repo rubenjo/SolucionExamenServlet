6a5e9b1b6080920fc97bf9e5d50e41f6f9cc6a2c
rubenjo <rubenxooo@gmail.com>
2017-12-20 17:52:55 +0100

    Repositorio Consolas

--- src/main/java/es/salesianos/repository/ConsolaRepository.java
+++ src/main/java/es/salesianos/repository/ConsolaRepository.java
@@ -2,7 +2,12 @@ package es.salesianos.repository;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import es.salesianos.connection.AbstractConnection;
 import es.salesianos.model.Consolas;
@@ -34,7 +39,9 @@ public class ConsolaRepository {
 		PreparedStatement preparedStatement=null;
 		try{
 			preparedStatement=conn.prepareStatement("UPDATE CONSOLAS SET "+"consola=?, nom_Empresa=? WHERE id=?");
-					
+			preparedStatement.setString(1, consoleFormulario.getConsola());
+			preparedStatement.setString(2, consoleFormulario.getNom_Empresa());
+			preparedStatement.setString(3, consoleFormulario.getId());
 		}catch(SQLException e){
 			e.printStackTrace();
 			throw new RuntimeException(e);
@@ -42,5 +49,55 @@ public class ConsolaRepository {
 			connection.close(preparedStatement);
 		}
 		connection.close(conn);
+	}
+	
+	public Optional<Consolas> search(Consolas console){
+		PreparedStatement preparedStatement=null;
+		ResultSet resultSet=null;
+		Connection conn=null;
+		Consolas maquineta=new Consolas();
+		try{		
+			conn=connection.open(jdbcUrl);
+			preparedStatement=conn.prepareStatement("SELECT * FROM CONSOLAS WHERE consola=?");
+			preparedStatement.setString(1, console.getConsola());
+			resultSet=preparedStatement.executeQuery();
+			while(resultSet.next()){	
+				maquineta.setConsola(resultSet.getString("consola"));
+				maquineta.setConsola(resultSet.getString("nom_Empresa"));
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}finally{
+			connection.close(preparedStatement);
+			connection.close(conn);
+		}
+		return Optional.ofNullable(maquineta);
+	}
+	
+	public List<Consolas> listAllConsoles(){
+		List<Consolas> listaConsolas=new ArrayList<Consolas>();
+		ResultSet resultSet=null;
+		Connection conn=null;
+		Statement statement=null;
+		Consolas maquineta=new Consolas();
+		try{
+			conn=connection.open(jdbcUrl);
+			statement=conn.createStatement();
+			resultSet=statement.executeQuery("SELECT * FROM CONSOLAS");
+			
+			while(resultSet.next()){
+				maquineta.setConsola(resultSet.getString("consola"));
+				maquineta.setNom_Empresa(resultSet.getString("nom_Empresa"));
+				listaConsolas.add(maquineta);
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}finally{
+			connection.close(statement);
+			connection.close(conn);
+		}
+		return listaConsolas;
 	}
 }
