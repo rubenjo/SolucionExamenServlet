de84478864834c00dcfb49124c49291f8ab0a485
rubenjo <rubenxooo@gmail.com>
2017-12-21 19:48:53 +0100

    Repositorio Videojuegos terminado

--- src/main/java/es/salesianos/repository/VideojuegoRepository.java
+++ src/main/java/es/salesianos/repository/VideojuegoRepository.java
@@ -2,7 +2,11 @@ package es.salesianos.repository;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Optional;
 import java.sql.Date;
 
 import es.salesianos.connection.AbstractConnection;
@@ -15,7 +19,7 @@ public class VideojuegoRepository {
 	private static final String jdbcUrl="jdbc:h2:file:./src/main/resources/test;INIT=RUNSCRIPT FROM 'classpath:scripts/create.sql'";
 	
 	public void insertar(Videojuegos videogamesFormulario){
-		Connection conn=null;
+		Connection conn=connection.open(jdbcUrl);
 		PreparedStatement preparedStatement=null;
 		try{
 			preparedStatement=conn.prepareStatement("INSERT INTO VIDEOJUEGOS(titulo,edadRecomendada,nom_Consola,fechaLanzamiento)"+"VALUES(?,?,?,?)");
@@ -28,7 +32,74 @@ public class VideojuegoRepository {
 			throw new RuntimeException(e);
 		}finally{
 			connection.close(conn);
+			connection.close(preparedStatement);
 		}
 	}
 	
+	public void update(Videojuegos videogamesFormulario){
+		Connection conn=connection.open(jdbcUrl);
+		PreparedStatement preparedStatement=null;
+		try{
+			preparedStatement=conn.prepareStatement("UPDATE VIDEOJUEGOS"+" SET edadRecomendada=?, nom_Consola=?, fechaLanzamiento=? WHERE titulo=?");
+			preparedStatement.setString(1, videogamesFormulario.getEdadRecomendada());
+			preparedStatement.setString(2, videogamesFormulario.getNom_Consola());
+			preparedStatement.setDate(3, new Date(videogamesFormulario.getFechaLanzamiento().getTime()));
+			preparedStatement.setString(4, videogamesFormulario.getTitulo());
+		}catch(SQLException e){
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}finally{
+			connection.close(conn);
+			connection.close(preparedStatement);
+		}
+	}
+	
+	public Optional<Videojuegos> search(Videojuegos games){
+		PreparedStatement preparedStatement=null;
+		ResultSet resultSet=null;
+		Connection conn=connection.open(jdbcUrl);
+		Videojuegos juegos=new Videojuegos();
+		try{
+			preparedStatement=conn.prepareStatement("SELECT * FROM VIDEOJUEGOS WHERE titulo=?");
+			preparedStatement.setString(1, games.getTitulo());
+			resultSet=preparedStatement.executeQuery();
+			while(resultSet.next()){
+				juegos.setEdadRecomendada(resultSet.getString("edadRecomendada"));
+				juegos.setFechaLanzamiento(resultSet.getDate("fechaLanzamiento"));
+				juegos.setNom_Consola(resultSet.getString("nom_Consola"));
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}finally{
+			connection.close(conn);
+			connection.close(preparedStatement);
+		}
+		return Optional.ofNullable(juegos);
+	}
+	
+	public List<Videojuegos> listAllGames(){
+		ResultSet resultSet=null;
+		Statement statement=null;
+		Connection conn=connection.open(jdbcUrl);
+		Videojuegos juegos=new Videojuegos();
+		try{
+			statement=conn.createStatement();
+			resultSet=statement.executeQuery("SELECT * FROM VIDEOJUEGOS");
+			while(resultSet.next()){
+				juegos.setTitulo(resultSet.getString("titulo"));
+				juegos.setEdadRecomendada(resultSet.getString("edadRecomendada"));
+				juegos.setFechaLanzamiento(resultSet.getDate("fechaLanzamiento"));
+				juegos.setNom_Consola(resultSet.getString("nom_Consola"));
+			}
+			
+		}catch(SQLException e){
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}finally{
+			connection.close(conn);
+			connection.close(statement);
+		}
+		return listAllGames();
+	}
 }
